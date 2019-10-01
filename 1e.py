a05440daaa805da3bb11384d72030a46c88f4065
mariusjessen <marius.jessen@gmail.com>
2019-10-01 21:26:32 +0200

    exercise 1e

--- 1e.py
+++ 1e.py
@@ -2,14 +2,60 @@ from functions import MSE,k_fold1,FrankeFunc,k_fold2,Ridge_sk_X
 
 import numpy as np
 import matplotlib.pyplot as plt
-
+'''
 #Initiating data
 np.set_printoptions(precision=4)
 
 #Datapoints - how many & DegreeMax
 
-D = 300
+D = 10000
 Deg_max = 5
+# Make data grid give same shape as x in exercise
+#np.random.seed(10)
+
+x1 = np.random.rand(D,1)
+y1 = np.random.rand(D,1)
+
+#x1 = x1.ravel()
+#y1 = y1.ravel()
+#print(x1.shape)
+
+#Sort before printing
+X = np.sort(x1,axis=0)
+Y = np.sort(y1,axis=0)
+#Create meshgrid for plotting
+#x_train,y_train = np.meshgrid(X, Y)
+
+#x, y = np.meshgrid(X,Y)
+
+#x1d = x.reshape((D**2, 1))
+#y1d = y.reshape((D**2, 1))
+
+#Create 1d arrays
+x1d = x1.ravel()
+y1d = y1.ravel()
+
+
+#Obtain true function
+#z = FrankeFunc(x_train,y_train)
+#True function or train function
+sigma = 0.5
+z_true = np.zeros(D)
+for i in range(D):
+    error = np.random.normal(0, sigma)
+    z_true[i] = FrankeFunc(x1d[i],y1d[i])+error
+#Initiate betas
+X = X.ravel()
+Y = Y.ravel()
+'''
+
+np.set_printoptions(precision=4)
+
+#Datapoints - how many & DegreeMax
+
+D = 70
+Deg_max = 5
+lamd = 0.0
 # Make data grid give same shape as x in exercise
 np.random.seed(10)
 
@@ -41,30 +87,38 @@ z = FrankeFunc(x_train,y_train)
 #True function or train function
 sigma = 0.5
 error = np.random.normal(0,sigma,D**2)
+
+print("error",error)
 z_true = FrankeFunc(x1d,y1d).ravel()+error
+
+print(z_true.shape)
+
 #Initiate betas
 X = X.ravel()
 Y = Y.ravel()
-
 
 #Find lambda
 folds = 5
 deg = 5
 i = 0
-lamdas = [0, 1, 2, 4, 5, 10,50,100,1000,10000,100000]
+#lamdas = [0, 1, 2, 4, 5, 10,50,100,1000,10**4,10**5,10**8,10**10,10**12,10**13,10**14]
+lamdas = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
 MSE_test = np.zeros(len(lamdas))
 variance= np.zeros(len(lamdas))
 bias= np.zeros(len(lamdas))
 MSE_train = np.zeros(len(lamdas))
 beta = np.zeros(len(lamdas))
 for lamd in lamdas:
-    MSE_test[i], MSE_train[i], bias[i], variance[i] = k_fold1(x1d, y1d, z_true, deg, folds, 'Ridge',lamd=lamd)
+    MSE_test[i], MSE_train[i], bias[i], variance[i] = k_fold2(x1d, y1d, z_true, deg, folds,'Ridge',lamd=lamd)
     i = i+1
+MSE_test = np.sqrt(MSE_test)
+MSE_train = np.sqrt(MSE_train)
 
 fig = plt.figure()
 line_test, = plt.plot(lamdas,MSE_test,label='TEST')
 line_train, = plt.plot(lamdas,MSE_train,label='TRAINING')
-plt.xscale('log')
+
+#plt.xscale('log')
 #plt.yscale('log')
 
 #line_test1, = plt.plot(Degrees,average_MSE_test2,label='TEST2')
